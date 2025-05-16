09f9c7184d4dcd5c55db19a09488eab0ac11dccd
Aguilar Matias Agustin <aguilar290520@gmail.com>
2025-05-16 19:56:48 -0300

    repository created

---
+++ src/main/java/com/example/KAV/KavApplication.java
@@ -0,0 +1,13 @@
+package com.example.KAV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KAV/KavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KAV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
